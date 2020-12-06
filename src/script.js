bc62d9b1fc7d1c72d92a0da1513e11e118f0db9b
Md Tazri <MdTazri@MdTazri>
2020-12-07 03:29:15 +0600

    I complate converter project in here.This app give number from user and convert binary,octal and hexadecimal.Thats So cool

---
+++ src/script.js
@@ -0,0 +1,42 @@
+//Track Element
+let input = document.getElementById('number');
+let btn = document.getElementById('submit-btn');
+let binaryField = document.getElementById('binary');
+let octalField = document.getElementById('octal');
+let hexadecimalField = document.getElementById('hexadecimal');
+
+//Converting function
+function action(event){
+    event.preventDefault();
+    let strNumber = input.value;
+    if(!strNumber){
+        alert('Please Provide Number');
+    }else{
+        let number = Number(strNumber);
+        //convert function
+        let binary = convert(number,2);
+        let octal = convert(number,8);
+        let hexa = convert(number,16)
+        
+        //append child
+        append(binaryField,binary);
+        append(octalField,octal);
+        append(hexadecimalField,hexa);
+    }
+}
+
+//add even listener
+btn.addEventListener('click',action);
+
+//converting funciton
+function convert(number,base){
+    return number.toString(base);
+}
+
+//append function
+function append(parent,number){
+    let p = document.createElement('p');
+    p.innerHTML = number;
+    parent.innerHTML = '';
+    parent.appendChild(p);
+}
